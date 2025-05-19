3784437a6246de3910fd9d21bfb6a55a82ce789f
Leonardo Lima <160889904+leojesusz@users.noreply.github.com>
2025-05-19 09:46:24 -0300

    conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,10 @@
 <?php
-// Caminho do banco de dados SQLite
 $db_path = __DIR__ . '/../db/database.sqlite';
 
 try {
     $pdo = new PDO("sqlite:" . $db_path);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Criação da tabela se não existir
     $pdo->exec("CREATE TABLE IF NOT EXISTS Inscrito (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
